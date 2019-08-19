dfb63435f1aa3272432c35ac957eb01971b73d21
Tim Dew <51726093+tadew85@users.noreply.github.com>
2019-08-19 14:01:32 -0400

    first commit created file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchCriteriaComponent } from './search-criteria/search-criteria.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { WatchListPageComponent } from './watch-list-page/watch-list-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchCriteriaComponent,
+    MovieListComponent,
+    WatchListPageComponent
   ],
   imports: [
     BrowserModule
