8f838fc260b475407cbeaa7cf8b357249d47ffd0
Sugene Mulpuri <sugene@Sugenes-MacBook-Pro-2.local>
2019-08-20 11:15:24 -0400

    environmentupdate

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,19 @@
-import { Injectable } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AppComponent } from './app.component';
+import { DisplayComponent } from './display/display.component';
 
-@Injectable({
-  providedIn: 'root'
+@NgModule({
+  declarations: [
+    AppComponent,
+    WatchListPageComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class MovieService {
-
-  constructor() { }
-}
+export class AppModule { }
