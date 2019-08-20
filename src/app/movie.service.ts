6b33668e2502ba8e2ba3185c0c3be25c6a8e334d
Tim Dew <51726093+tadew85@users.noreply.github.com>
2019-08-20 11:13:37 -0400

    8/20 changes

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+getMovieData(searchTerm: string): Observable<any>{
+  return this.http.get(`https://api.themoviedb.org/3/movie/550?api_key=db461ada2c7443f6ccd1e6ad86adde8d&q=${searchTerm}&limit=25&offset=0&rating=G&lang=en`)
+}
+
 }
